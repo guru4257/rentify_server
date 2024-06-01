2d77bb5e16bc5b04acdc3356ad7d5b079735da0e
GuruprasathManikandan <guruprasath@saptanglabs.com>
2024-06-01 08:45:28 +0530

    removed verify token middleware

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,10 +6,10 @@ import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.get('/test', test);
-router.post('/update/:id', verifyToken, updateUser)
-router.delete('/delete/:id', verifyToken, deleteUser)
-router.get('/listings/:id', verifyToken, getUserListings)
-router.get('/getDetails/:id', verifyToken, getUser)
-router.post('/sendEmail/',verifyToken,sendMail)
+router.post('/update/:id', updateUser)
+router.delete('/delete/:id', deleteUser)
+router.get('/listings/:id', getUserListings)
+router.get('/getDetails/:id', getUser)
+router.post('/sendEmail/',sendMail)
 
 export default router;
